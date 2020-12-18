5ce5543af6b9af30d4c05b24556b95e1357d72a0
Jason Eliasen <eliasenjason@gmail.com>
2020-12-18 05:55:08 -0700

    jumbo container loading data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Jumbotron from '../components/jumbotron';
+import { JumbotronContainer } from '../containers/jumbotron'
+
 
 export default function Home() {
     return (
-        <Jumbotron.Container>
-            <Jumbotron.Title>This is the Title</Jumbotron.Title>
-            <Jumbotron.SubTitle>this is the subtitle</Jumbotron.SubTitle>
-        </Jumbotron.Container>
+        <JumbotronContainer />
     )
 }
